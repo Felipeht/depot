c3c67e3f54d66167b9ac26f9ec6de4516c97e3ab
Felipe Hamamoto Toyoda <felipe.toyoda@codus.com.br>
2013-05-15 13:30:32 -0300

    criado o rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'bf0ce90b62a227b7403b208517ce8a3a78d83d88a9d7b9fe5dd15af1fb356a7af0ce0172d917728e9b6a6c86ab2e07c18b6efb82ca879121233899c53882f6a1'
